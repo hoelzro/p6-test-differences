5a209511f32fa1291b361eafa415268c579ba290
Rob Hoelz <rob@hoelz.ro>
2014-08-09 22:10:17 -0500

    Add basic implementation of eq-or-diff

--- lib/Test/Differences.pm
+++ lib/Test/Differences.pm
@@ -1,7 +1,43 @@
 use v6;
 use Algorithm::LCS;
+use Test;
 
 module Test::Differences:vers<0.1.0>:auth<hoelzro> {
-    our sub eq-or-diff(@got, @expected, Str $reason?, Int :$start-index = 0) {
+    # XXX test level
+    our sub eq-or-diff(@got, @expected, Str $reason?, Int :$start-index = 0) is export {
+        my &compare = &infix:<eqv>;
+        my @lcs = lcs(@got, @expected, :&compare);
+
+        if +@lcs == +@got {
+            pass $reason;
+            return True;
+        }
+        flunk $reason;
+
+        my $lcs-index      = 0;
+        my $got-index      = 0;
+        my $expected-index = 0;
+
+        while $lcs-index      < @lcs ||
+              $got-index      < @got ||
+              $expected-index < @expected {
+
+            my $head          = @lcs[$lcs-index];
+            my $got-head      = @got[$got-index];
+            my $expected-head = @expected[$expected-index];
+
+            if &compare($head, $got-head) && &compare($head, $expected-head) {
+                diag("\@got[$got-index] and \@expected[$expected-index] are equal");
+                $_++ for $lcs-index, $got-index, $expected-index;
+            } elsif &compare($head, $got-head) {
+                diag("deleted \@expected[$expected-index]");
+                $expected-index++;
+            } elsif &compare($head, $expected-head) {
+                diag("added \@got[$got-index]");
+                $got-index++;
+            }
+        }
+
+        False
     }
 }
